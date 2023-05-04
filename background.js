d217afaaea0b19032dc5f53ee0749bb7b601ddd7
Mahmoud Ashraf <mahmoud55551226@yahoo.com>
2023-05-04 03:55:29 +0300

    تغييرات جذرية للإضافة change azker.js file , background.js , logo , manifest file

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 import azkar from "./azkar.js"
 
 const alarmName = "zekr-reminder"
+
 chrome.runtime.onInstalled.addListener(() => {
     sendNotification()
     chrome.alarms.get(alarmName, alarm => {
@@ -38,14 +39,46 @@ function clearAlarm() {
 }
 
 function sendNotification() {
-    const zekr = azkar[Math.floor(Math.random() * azkar.length)];
-    const notifications_title = "Zekr Reminder"
+    const now = new Date();
+    const hourOfDay = now.getHours();
+    const isMorningTime = hourOfDay >= 5 && hourOfDay < 16;
+    const isEveningTime = hourOfDay >= 20 || hourOfDay < 5;
+
+    let zekr = null;
+    let lastZekr = null;
+    let tag = null;
+
+    if (isMorningTime) {
+        tag = 'أذكار الصباح';
+    } else if (isEveningTime) {
+        tag = 'أذكار المساء';
+    }
+
+    if (tag === null) {
+        return;
+    }
+
+    const filteredAzkar = azkar.filter(zekr => zekr.tag === tag);
+    if (filteredAzkar.length === 0) {
+        return;
+    }
+
+    do {
+        zekr = filteredAzkar[Math.floor(Math.random() * filteredAzkar.length)];
+    } while (zekr === lastZekr);
+    lastZekr = zekr;
+
     chrome.notifications.create({
         type: "basic",
-        iconUrl: "./assets/images/logo.png",
-        title: notifications_title,
-        message: zekr,
+        iconUrl: "./assets/images/tasbih.png",
+        title: "", // Use the tag property instead of category
+        message: zekr.text,
+        contextMessage: zekr.bless,
         priority: 2,
         silent: false,
+        requireInteraction: true,
     });
+    setTimeout(() => {
+        chrome.notifications.clear(tag); // Clear the notification after a minute
+    }, 5000);
 }
